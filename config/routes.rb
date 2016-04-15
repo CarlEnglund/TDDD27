89b1a1b297ec6fe79c8cbe4b4fd6879e7471470d
Carl Englund <caren083@student.liu.se>
2016-04-15 15:07:42 +0200

    Fixed redirect from landingpage to home and removed old unnecessary link.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
    root 'pages#landing'
 
+   get '/home', to: 'pages#home'
+
    get '/auth/:provider/callback', to: 'sessions#create'
 
    delete '/logout', to: 'sessions#destroy'
