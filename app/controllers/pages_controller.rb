6449f64e33346e7206d003565da8713a19dc0622
Carl Englund <caren083@student.liu.se>
2016-05-10 15:14:55 +0200

    Changed pages controller to return last 5 reviews

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   def home
     @user = current_user
-    @reviews = Review.all
+    @reviews = Review.last(5)
+    @userReviews = Review.where(user_id: current_user.id)
+
   end
   def about
     @user = current_user
