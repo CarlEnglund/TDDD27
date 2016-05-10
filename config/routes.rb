c12d7ef7f6df11b070fb166c6e3e3c5df618e30d
Carl Englund <caren083@student.liu.se>
2016-05-10 15:15:38 +0200

    Updated some routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
    get '/auth/:provider/callback', to: 'sessions#create'
 
-   delete '/logout', to: 'sessions#destroy'
+   get '/logout', to: 'sessions#destroy'
 
   get '/search', to: 'search#index'
 
