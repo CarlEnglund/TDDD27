fbf03da00176bd3887e38993283a59676a66f55f
Carl Englund <caren083@student.liu.se>
2016-05-10 15:13:51 +0200

    Added components for main box and profile box, also showing recent reviews

---
+++ app/assets/javascripts/components/recent_review_list.js.jsx
@@ -0,0 +1,63 @@
+var hej = null;
+var RecentReviewList = React.createClass({
+
+    getInitialState: function() {
+        return {
+            poster: [],
+            name: [],
+
+        };
+    },
+
+    componentWillMount: function() {
+
+        var b = [];
+        this.props.data.forEach(function(data) {
+            data.forEach(function(map) {
+                b.push('https://api.themoviedb.org/3/movie/' + map.movie_id + '?api_key=404f72b9ac53b34084b290e2f2ddabbc');
+            })
+
+        })
+
+        for(var data in b)
+        {
+            fetch(b[data]).then((response) => response.json())
+                .then((responseData) => {
+                    this.setState({
+                        poster: this.state.poster.concat(["http://image.tmdb.org/t/p/w154/" + responseData.poster_path]),
+                        name: this.state.name.concat([responseData.original_title])
+
+                    })
+                })
+        }
+    },
+
+    render: function() {
+
+        var array = []
+        var array2 = []
+        this.props.data.map(function(data) {
+            array.push(data);
+        })
+        this.state.name.map(function(data) {
+            array2.push(data);
+        })
+        console.log(array2);
+        return (
+
+            <div>{this.state.poster.map(function(data, key){
+                return (<PosterList poster={data} name={array2[key]} ></PosterList>)
+            })
+            }
+
+            </div>
+        );
+    },
+});
+
+var PosterList = React.createClass({
+    render: function() {
+
+        return <div><img src={this.props.poster}/><p>{this.props.name}</p></div>
+    }
+})
